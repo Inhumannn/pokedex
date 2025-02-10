49b975b5fe77fb5b56bdb5c3529e9858ee00b215
Inhumannn <thomas.pnbm@gmail.com>
2025-02-10 16:14:09 +0100

    refactor(pokedex): remove evolution field
    - Removed unnecessary evolution field
    - Adjusted combobox placement
    - Added default values to comboboxes
    - Renamed "envoyer" button to "enregistrer"
    - Updated combobox values

--- pokedex.py
+++ pokedex.py
@@ -3,10 +3,9 @@ from PIL import ImageTk, Image
 from tkinter import ttk
 
 class Pokemon:
-    def __init__(self, name, types, evolution, heal, attack, speed, defence, weakness):
+    def __init__(self, name, types, heal, attack, speed, defence, weakness):
         self,name = name
         self.types = types
-        self.evolution = evolution
         self.heal = heal
         self.attack = attack
         self.speed = speed
@@ -66,23 +65,30 @@ style.configure("TCombobox", fieldbackground="red", background="red", borderwidt
 # .....
 # Combobox
 types = ttk.Combobox(root)
-types.place(x=720, y=310)
-evolution = ttk.Combobox(root)
-evolution.place(x=720, y=335)
+types.set("Types ")
+types.place(x=720, y=335)
+name = ttk.Combobox(root)
+name.set("Name")
+name.place(x=720, y=310)
 heal = ttk.Combobox(root)
+heal.set("Heal")
 heal.place(x=720, y=360)
 attack = ttk.Combobox(root)
+attack.set("Attack")
 attack.place(x=720, y=385)
 speed = ttk.Combobox(root)
+speed.set("Speed")
 speed.place(x=720, y=410)
 defence = ttk.Combobox(root)
+defence.set("Defence")
 defence.place(x=720, y=435)
 weak = ttk.Combobox(root)
+weak .set("Weak")
 weak.place(x=720, y=460)
+# name
+name['values'] = ("option 1", "option 2")
 # Types
 types['values'] = ("Normal", "Feu", "Eau", "Plante", "Électrik", "Glace", "Combat", "Poison", "Sol", "Vol", "Psy", "Insecte", "Roche", "Fantôme", "Dragon", "Ténèbres", "Acier", "Fée")
-# evolution
-evolution['values'] = ("option 1", "option 2")
 # heal
 heal['values'] = (" 50-70", "70-90", "90-120", "120+")
 # attack
@@ -94,7 +100,7 @@ defence['values'] = ("40-60", "60-80", "80-100", "100+")
 # weak
 weak['values'] = ("option 1", "option 2")
 # Bouton Enregistrer
-envoyer = Button(root, text="Enregistrer", borderwidth=0, bg="red")
-envoyer.place(x=764, y=485)
+enregistrer = Button(root, text="Enregistrer", borderwidth=0, bg="red")
+enregistrer.place(x=764, y=485)
 # Fin
 root.mainloop()
