b8162855c050adbebd5897c996912e6069f4d184
Inhumannn <thomas.pnbm@gmail.com>
2025-02-10 16:53:45 +0100

    refactor(UI): Improve Pokemon display
    - Replaced Text widget with Label
    - Added Pokemon list with sample data
    - Improved combobox styling
    - Replaced some comboboxes with Entry
    - Adjusted button positions

--- pokedex.py
+++ pokedex.py
@@ -21,11 +21,14 @@ root.tk.call('wm','iconphoto', root._w, PhotoImage(file="img/icon.png"))
 # Taille
 root.geometry("900x600")
 ## Gauche
-name = Text(root, height=4, width=50)
-name.pack()
-text = "#000 Pokemon"
-name.insert(END, text)
-
+# Logo pokemon
+name_pokemon = Frame(root)
+name_pokemon.pack()
+# Positionnement
+name_pokemon.place(anchor=NE , relx=0.5, rely=0.5)
+# Label
+label = Label(name_pokemon, text="blabla", borderwidth=0, highlightthickness=0)
+label.pack() ## Continuer ne marche pas encore
 
 ## Droite
 # Frame : pokemon_profile
@@ -53,23 +56,28 @@ frame.place(x=725, y=100)
 lb = Listbox(frame, bg="red", borderwidth=0, highlightthickness=0)
 lb.pack()
 # Element de liste
-lb.insert(END, "blabla")
+lb.insert(END, "Bulbizarre")
+lb.insert(END, "Salamèche")
+lb.insert(END, "Carapuce")
+lb.insert(END, "Pikachu")
 # Bouton Envoyer
 envoyer = Button(root, text="Selectionner", borderwidth=0, bg="red")
-envoyer.place(x=764, y=270)
+envoyer.place(x=755, y=270)
 # Styles cbb
-style = ttk.Style()
+style = ttk.Style(root)
 style.theme_use('clam')
-style.configure("TCombobox", fieldbackground="red", background="red", borderwidth=0)
+style.configure("TCombobox", fieldbackground="red", background="red", borderwidth=65)
 # Border
 # .....
 # Combobox
 types = ttk.Combobox(root)
 types.set("Types ")
 types.place(x=720, y=335)
-name = ttk.Combobox(root)
-name.set("Name")
-name.place(x=720, y=310)
+# name
+name = Entry(root, bg="Red")
+name.insert(0, "Name")
+name.place(x=727, y=310)
+#
 heal = ttk.Combobox(root)
 heal.set("Heal")
 heal.place(x=720, y=360)
@@ -82,11 +90,10 @@ speed.place(x=720, y=410)
 defence = ttk.Combobox(root)
 defence.set("Defence")
 defence.place(x=720, y=435)
-weak = ttk.Combobox(root)
-weak .set("Weak")
-weak.place(x=720, y=460)
-# name
-name['values'] = ("option 1", "option 2")
+# Weak
+weak = Entry(root, bg="Red")
+weak.insert(0, "Weak")
+weak.place(x=727, y=460)
 # Types
 types['values'] = ("Normal", "Feu", "Eau", "Plante", "Électrik", "Glace", "Combat", "Poison", "Sol", "Vol", "Psy", "Insecte", "Roche", "Fantôme", "Dragon", "Ténèbres", "Acier", "Fée")
 # heal
@@ -97,10 +104,8 @@ attack['values'] = ("45-60", "60-80", "80-100", "100-150+")
 speed['values'] = ("50-70", "70-90", "90-120", "120+")
 # defence
 defence['values'] = ("40-60", "60-80", "80-100", "100+")
-# weak
-weak['values'] = ("option 1", "option 2")
 # Bouton Enregistrer
 enregistrer = Button(root, text="Enregistrer", borderwidth=0, bg="red")
-enregistrer.place(x=764, y=485)
+enregistrer.place(x=758, y=485)
 # Fin
 root.mainloop()
