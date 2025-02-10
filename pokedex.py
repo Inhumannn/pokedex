6d48bdb44b07b2758aa8906dc26a879dabbc76ce
Inhumannn <thomas.pnbm@gmail.com>
2025-02-10 15:59:26 +0100

    feat(pokedex): enhance UI and add features
    - Added image logo and icon
    - Implemented combobox for filtering
    - Improved pokemon data model
    - Added input fields for pokemon details
    - Created save button for data

--- pokedex.py
+++ pokedex.py
@@ -1,9 +1,11 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from tkinter import ttk
 
 class Pokemon:
-    def __init__(self, type, evolution, heal, attack, speed, defence, weakness):
-        self.type = type
+    def __init__(self, name, types, evolution, heal, attack, speed, defence, weakness):
+        self,name = name
+        self.types = types
         self.evolution = evolution
         self.heal = heal
         self.attack = attack
@@ -11,11 +13,22 @@ class Pokemon:
         self.defence = defence
         self.weakness = weakness
     
+## Principale
 root = Tk()
 # Titre
 root.title("Pokedex")
+# Logo
+root.tk.call('wm','iconphoto', root._w, PhotoImage(file="img/icon.png"))
 # Taille
 root.geometry("900x600")
+## Gauche
+name = Text(root, height=4, width=50)
+name.pack()
+text = "#000 Pokemon"
+name.insert(END, text)
+
+
+## Droite
 # Frame : pokemon_profile
 pokemon_profile = Frame(root, bg="deep sky blue", width=675, height=600, highlightbackground="Black", highlightthickness=7)
 # positionnement
@@ -27,9 +40,61 @@ type_pokemon.place(x=670, y=0)
 # Logo pokemon
 place_logo = Frame(root)
 place_logo.pack()
+# Positionnement
 place_logo.place(anchor=NE , relx=0.96, rely=0.05)
-
+# Affichage
 logo = ImageTk.PhotoImage(Image.open("img/logo.png"))
-label = Label(place_logo, image=logo)
+# Label
+label = Label(place_logo, image=logo, borderwidth=0, highlightthickness=0)
 label.pack()
+# Positionnnement lb
+frame = Frame(root)
+frame.place(x=725, y=100)
+# Menu déroulant
+lb = Listbox(frame, bg="red", borderwidth=0, highlightthickness=0)
+lb.pack()
+# Element de liste
+lb.insert(END, "blabla")
+# Bouton Envoyer
+envoyer = Button(root, text="Selectionner", borderwidth=0, bg="red")
+envoyer.place(x=764, y=270)
+# Styles cbb
+style = ttk.Style()
+style.theme_use('clam')
+style.configure("TCombobox", fieldbackground="red", background="red", borderwidth=0)
+# Border
+# .....
+# Combobox
+types = ttk.Combobox(root)
+types.place(x=720, y=310)
+evolution = ttk.Combobox(root)
+evolution.place(x=720, y=335)
+heal = ttk.Combobox(root)
+heal.place(x=720, y=360)
+attack = ttk.Combobox(root)
+attack.place(x=720, y=385)
+speed = ttk.Combobox(root)
+speed.place(x=720, y=410)
+defence = ttk.Combobox(root)
+defence.place(x=720, y=435)
+weak = ttk.Combobox(root)
+weak.place(x=720, y=460)
+# Types
+types['values'] = ("Normal", "Feu", "Eau", "Plante", "Électrik", "Glace", "Combat", "Poison", "Sol", "Vol", "Psy", "Insecte", "Roche", "Fantôme", "Dragon", "Ténèbres", "Acier", "Fée")
+# evolution
+evolution['values'] = ("option 1", "option 2")
+# heal
+heal['values'] = (" 50-70", "70-90", "90-120", "120+")
+# attack
+attack['values'] = ("45-60", "60-80", "80-100", "100-150+")
+# speed
+speed['values'] = ("50-70", "70-90", "90-120", "120+")
+# defence
+defence['values'] = ("40-60", "60-80", "80-100", "100+")
+# weak
+weak['values'] = ("option 1", "option 2")
+# Bouton Enregistrer
+envoyer = Button(root, text="Enregistrer", borderwidth=0, bg="red")
+envoyer.place(x=764, y=485)
+# Fin
 root.mainloop()
